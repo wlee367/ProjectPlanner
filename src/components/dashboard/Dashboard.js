c492de0a0cf72629dd709148400ebc0e0f2849a8
Won Ryul Lee <wlee367@uwo.ca>
2019-03-29 00:47:42 -0400

    Added notifications cloud functions, wired it up to the components via redux, deployed firebase

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -9,7 +9,7 @@ import { Redirect } from 'react-router-dom';
 class Dashboard extends Component {
     render() {
         // console.log(this.props);
-        const { projects, auth } = this.props;
+        const { projects, auth, notifications } = this.props;
 
         if (!auth.uid) {
             return <Redirect to="/signin" />;
@@ -22,7 +22,7 @@ class Dashboard extends Component {
                         <ProjectList projects={projects} />
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications />
+                        <Notifications notifications={notifications} />
                     </div>
                 </div>
             </div>
@@ -34,7 +34,8 @@ const mapStateToProps = state => {
     // console.log(state);
     return {
         projects: state.firestore.ordered.projects,
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
     };
 };
 
@@ -43,5 +44,12 @@ export default compose(
         mapStateToProps,
         null
     ),
-    firestoreConnect([{ collection: 'projects' }])
+    firestoreConnect([
+        { collection: 'projects', orderBy: ['createdAt', 'desc'] },
+        {
+            collection: 'notifications',
+            limit: 3,
+            orderBy: ['time', 'desc']
+        }
+    ])
 )(Dashboard);
